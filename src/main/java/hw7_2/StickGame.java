8ad70563ce8588429f99a72272c00fef8848d71d
TanyaB1460 <bobrova1406@gmail.com>
2025-06-13 17:42:15 +0300

    .

---
+++ src/main/java/hw7_2/StickGame.java
@@ -0,0 +1,35 @@
+package hw7_2;
+import java.util.*;
+
+public class StickGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] moves = new int[n+1][];
+        moves[0] = new int[0];
+
+        for (int i = 1; i <= n; i++) {
+            int k = sc.nextInt();
+            moves[i] = new int[k];
+            for (int j = 0; j < k; j++) {
+                moves[i][j] = sc.nextInt();
+            }
+        }
+
+        boolean[] isWinning = new boolean[n+1];
+        isWinning[0] = false;
+
+        for (int i = 1; i <= n; i++) {
+            for (int move : moves[i]) {
+                if (i - move >= 0 && !isWinning[i - move]) {
+                    isWinning[i] = true;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(isWinning[n] ? "First" : "Second");
+    }
+}
